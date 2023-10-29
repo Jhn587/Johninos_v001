bb1e40bdb578386371d560686985b15fdb43ec8b
John French <jhfren@amazon.com>
2023-10-29 10:39:33 -0400

    Adding form to front page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+ PizzaOrderCreateForm 
+} from './ui-components';
+
 
 function App() {
   return (
     <div className="App">
+      <PizzaOrderCreateForm
+        onSubmit={(fields) => {
+            // Example function to trim all string inputs
+            const updatedFields = {}
+            Object.keys(fields).forEach(key => {
+                if (typeof fields[key] === 'string') {
+                    updatedFields[key] = fields[key].trim()
+                } else {
+                    updatedFields[key] = fields[key]
+                }
+            })
+            return updatedFields
+        }}
+    />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
